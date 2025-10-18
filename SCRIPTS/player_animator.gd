24a7727b14a520ba6ed881007a6a8e117260484b
Gaburino <143316156+Gaburagi@users.noreply.github.com>
2025-10-18 14:57:39 +0800

    Fixed animation latency

--- SCRIPTS/player_animator.gd
+++ SCRIPTS/player_animator.gd
@@ -5,17 +5,18 @@ extends Node2D
 @export var sprite : Sprite2D
 
 func _process(delta):
+	# Flip sprite based on direction
 	if player_controller.direction == 1:
 		sprite.flip_h = false
 	elif player_controller.direction == -1:
 		sprite.flip_h = true
-		
-		if abs(player_controller.velocity.x) > 0.0:
-			animation_player.play("run")
-		else:
-			animation_player.play("idle")
-			
-		if player_controller.velocity.y < 0.0:
-			animation_player.play("jump")
-		elif player_controller.velocity.y > 0.0:
-			animation_player.play  ("fall")
+
+	# Determine animation based on velocity
+	if player_controller.velocity.y < 0.0:
+		animation_player.play("jump")
+	elif player_controller.velocity.y > 0.0:
+		animation_player.play("fall")
+	elif abs(player_controller.velocity.x) > 0.0:
+		animation_player.play("run")
+	else:
+		animation_player.play("idle")
